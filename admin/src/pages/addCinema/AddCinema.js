8e0c6cb7a1bc26cc7ac6135e2c8727ccaa51bd70
Daxit1234 <dakshitgodhani103@gmail.com>
2024-01-19 19:17:57 +0530

    19-01 addcinema

--- admin/src/pages/addCinema/AddCinema.js
+++ admin/src/pages/addCinema/AddCinema.js
@@ -1,13 +1,59 @@
-import React from 'react'
-import SideBar from '../../components/sideBar/SideBar'
-
+import React from "react";
+import SideBar from "../../components/sideBar/SideBar";
+import Header2 from "../../components/header2/Header2";
+import "./AddCinema.css";
+import cinema from "./cinema.json";
+import AddCinemaModel from "../../components/addCinemaModel/AddCinemaModel";
 const AddCinema = () => {
+  console.log(cinema);
   return (
-    <div className='d-flex'>
-      <SideBar/>
-      <h1 className=''>add cinema</h1>
+    <div className="d-flex">
+      <SideBar />
+      <div className="w-100">
+        <Header2 page="Cinema List" />
+        <div className="d-flex justify-content-between">
+          <div className="h3 opacity-25 m-3">Cinema List</div>
+          <div className="p-3">
+            <button type="button" class="btn btn-primary btn-lg"   data-toggle="modal"
+              data-target="#exampleModalCenter">
+              Add Cinema
+            </button>
+          </div>
+        </div>
+        <div className="cinema-list">
+          <table className="table w-100 overflow-auto table-striped">
+            <tr className="table-title">
+              <th>c.No</th>
+              <th>Cinema Name</th>
+              <th>Address</th>
+              <th>City</th>
+              <th>Location</th>
+            </tr>
+            {cinema.results.map((item, index) => {
+              return (
+                <tr className={`${index % 2 === 0 ? "even-row" : "odd-row"}`}>
+                  <th>{index + 1}</th>
+                  <td>{item.cinema}</td>
+                  <td>{item.address}</td>
+                  <td>{item.city}</td>
+                  <td className="p-1">
+                    <iframe
+                      src={item.location}
+                      width="150"
+                      height="70"
+                      loading="lazy"
+                      className="bg-dark"
+                    ></iframe>
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      </div>
+      <AddCinemaModel />
     </div>
-  )
-}
+  );
+};
 
-export default AddCinema
+export default AddCinema;
--- admin/src/pages/dashboard/Dashboard.js
+++ admin/src/pages/dashboard/Dashboard.js
@@ -1,24 +1,60 @@
-import React from 'react'
-import "./Dashboard.css"
-import SideBar from '../../components/sideBar/SideBar'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import "./Dashboard.css";
+import SideBar from "../../components/sideBar/SideBar";
+import { useLocation } from "react-router-dom";
+import Header2 from "../../components/header2/Header2";
 
 function Dashboard() {
-  const location=useLocation();
-  console.log(location)
+  const location = useLocation();
+  console.log(location);
   return (
     <>
-    <div className='d-flex'>
-      <SideBar/>
-      <div className='counting-box'>
-        <div className='users'></div>
-        <div className='cinema'></div>
-        <div className='shows'></div>
-        <div className="booking"></div>
+      <div className="d-flex">
+        <SideBar />
+        <div className=" w-100">
+        <Header2  page="DashBoard"/>
+        <div className="counting-box">
+          <div className="users m-3 counting-item">
+            <div>
+              <div className="count-no" >24</div>
+              <div  className="title-item">Users</div>
+            </div>
+            <div className="d-flex">
+            <i className="fa-solid fa-user"></i>
+            </div>
+          </div>
+          <div className="cinema m-3 counting-item">
+            <div>
+            <div className="count-no">24</div>
+              <div className="title-item">Cinema</div>
+            </div>
+            <div className="d-flex">
+            <i class="fa-solid fa-chart-simple"></i>
+            </div>
+          </div>
+          <div className="shows m-3 counting-item">
+            <div>
+            <div className="count-no">24</div>
+              <div className="title-item">Shows</div>
+            </div>
+            <div className="d-flex">
+            <i class="fa-solid fa-video"></i>
+            </div>
+          </div>
+          <div className="booking m-3 counting-item">
+            <div>
+            <div className="count-no">24</div>
+              <div className="title-item">Bookings</div>
+            </div>
+            <div className="d-flex">
+            <i class="fa-solid fa-chart-pie"></i>
+            </div>
+          </div>
+        </div>
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
